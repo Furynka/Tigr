6ec38c12a4010785aee8f4e46530c6cedd7facdc
Khudiakov Daniil <khudiakov@outlook.com>
2016-10-23 14:38:21 +0200

    added worker entity

---
+++ persistence/src/main/java/com/dreamteam/entity/Worker.java
@@ -0,0 +1,42 @@
+package com.dreamteam.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+/**
+ * Entity class representing animals.
+ *
+ * @author Daniil Khudiakov
+ */
+@Entity
+@Table(name="Workers")
+public class Worker{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable=false,unique=true)
+    @Pattern(regexp=".+@.+\\....?")
+    @NotNull
+    private String email;
+
+    @NotNull
+    private String password;
+
+    private boolean administrator;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isAdministrator() {
+        return this.administrator;
+    }
+}
