1086239aa8998c5c4216d0706d597eaadbe23865
Evka <evka@localhost.localdomain>
2016-10-24 20:43:43 +0200

    Class Environment: hashCode method implemented

--- persistence/src/main/java/com/dreamteam/entity/Environment.java
+++ persistence/src/main/java/com/dreamteam/entity/Environment.java
@@ -33,4 +33,21 @@ public class Environment {
         this.name = name;
     }
 
+   /* @Override
+    public boolean equals(Object o){
+
+    }*/
+
+    @Override
+    public int hashCode(){
+        final int prime = 31;
+        int result = 1;
+
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + id;
+
+        return result;
+    }
+
+
 }
