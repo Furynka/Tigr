a259c06ce97dababfe980d2a020066d0a78000de
Evka <evka@localhost.localdomain>
2016-10-26 19:14:51 +0200

    Added implementation to classes EnvironmentDao and EnvironmentDaoImpl

--- persistence/src/main/java/com/dreamteam/dao/EnvironmentDaoImpl.java
+++ persistence/src/main/java/com/dreamteam/dao/EnvironmentDaoImpl.java
@@ -1,7 +1,52 @@
 package com.dreamteam.dao;
 
+import com.dreamteam.entity.Environment;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
 /**
  * @author Eva Ambrusova
  */
-public class EnvironmentDaoImpl {
+public class EnvironmentDaoImpl implements EnvironmentDao {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public void create(Environment e) {
+        em.persist(e);
+    }
+
+    @Override
+    public Environment findById(int id) {
+        return em.find(Environment.class, id);
+    }
+
+    @Override
+    public Environment findByName(String name) {
+        try{
+            return em.createQuery("SELECT e FROM Environment e WHERE name = :name", Environment.class)
+                    .setParameter(":name", name).getSingleResult();
+        } catch (NoResultException ex){
+            return null;
+        }
+    }
+
+    @Override
+    public List<Environment> findAll() {
+        return em.createQuery("SELECT e FROM Environment e", Environment.class).getResultList();
+    }
+
+    @Override
+    public Environment update(Environment e) {
+        return em.merge(e);
+    }
+
+    @Override
+    public void delete(Environment e) {
+        em.remove(e);
+    }
 }
