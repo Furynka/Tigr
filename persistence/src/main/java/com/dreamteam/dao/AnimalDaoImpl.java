5cabd1c24e8fe947906a40ad8d1ca09fe2cba9ba
Furynka <o.krysa@seznam.cz>
2016-10-29 16:46:12 +0200

    created implementation class for AnimalDao

---
+++ persistence/src/main/java/com/dreamteam/dao/AnimalDaoImpl.java
@@ -0,0 +1,63 @@
+package com.dreamteam.dao;
+
+import com.dreamteam.entity.Animal;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Access class for Animal entities.
+ * @author Jiri Oliva
+ */
+@Repository
+public class AnimalDaoImpl implements AnimalDao{
+
+    @PersistenceContext
+    private EntityManager entityManager;
+    
+    @Override
+    public void create(Animal animal) {
+        entityManager.persist(animal);
+    }
+
+    @Override
+    public void delete(Animal animal) {
+        entityManager.remove(animal);
+    }
+
+    @Override
+    public void update(Animal animal) {
+        entityManager.merge(animal);
+    }
+
+    @Override
+    public Animal findById(Long id) {
+        return entityManager.find(Animal.class, id);
+    }
+
+    @Override
+    public Animal findByName(String name) {
+        if (name == null || name.isEmpty())
+            throw new IllegalArgumentException("Received invalid name");
+
+        try {
+            return entityManager.createQuery(
+                    "select a from Animal a where a.name=:name", Animal.class)
+                    .setParameter("name", name)
+                    .getSingleResult();
+        } catch (NoResultException ex) {
+            return null;
+        }
+    }
+
+    @Override
+    public List<Animal> getAll() {
+        TypedQuery<Animal> query = entityManager.createQuery(
+                "SELECT a FROM Animal a", Animal.class);
+        return query.getResultList();
+    }
+    
+}
