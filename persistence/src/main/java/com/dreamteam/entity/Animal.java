31880a1ecb68f6e6f50d0bd5f861e430bedbb74d
Furynka <o.krysa@seznam.cz>
2016-10-29 15:37:10 +0200

    added hashCode adnd equals methods

--- persistence/src/main/java/com/dreamteam/entity/Animal.java
+++ persistence/src/main/java/com/dreamteam/entity/Animal.java
@@ -90,4 +90,31 @@ public class Animal{
     public void setEnviroments(Set<Enviroment> enviroments) {
         this.enviroments = enviroments;
     }
+    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Animal other = (Animal) obj;
+        if ((this.name == null) ? (other.name != null) : !this.name.equals(other.name)) {
+            return false;
+        }
+        return true;
+    }
+      
 }
