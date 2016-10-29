0789e2cecf22315cc95f2731e0d41d509af7213c
Furynka <o.krysa@seznam.cz>
2016-10-29 16:27:26 +0200

    created access interface for Animal

---
+++ persistence/src/main/java/com/dreamteam/dao/AnimalDao.java
@@ -0,0 +1,50 @@
+package com.dreamteam.dao;
+
+import com.dreamteam.entity.Animal;
+import java.util.Set;
+
+/**
+ * Access interface for Animal entities.
+ * 
+ * @author Jiri Oliva
+ */
+public interface AnimalDao {
+    
+    /**
+     * Creates new Animal entity.
+     * @param animal new Animal entity
+     */
+    public void create(Animal animal);
+    
+    /**
+     * Deletes existing Animal entity.
+     * @param animal Animal entity to remove
+     */
+    public void delete(Animal animal);
+    
+    /**
+     * Updates existing Animal entity;
+     * @param animal Animal entity to update
+     */
+    public void update(Animal animal);
+    
+    /**
+     * Finds Animal entity by its Id.
+     * @param id
+     * @return Animal or null, if not existing
+     */
+    public Animal findById(Long id);
+    
+    /**
+     * Finds Animal entity by its name.
+     * @param name
+     * @return Animal or null, if not existing
+     */
+    public Animal findByName(String name);
+    
+    /**
+     * Returns all existing Animal entitites.
+     * @return Set filled with existing Animal entities.
+     */
+    public Set<Animal> getAll();
+}
