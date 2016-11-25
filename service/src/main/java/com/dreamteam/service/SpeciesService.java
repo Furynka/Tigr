d36a52570a139451007946f0ba596d34cdf7fb1c
theJanNovak <novanojj@gmail.com>
2016-11-25 15:42:19 +0100

    Species Facade

---
+++ service/src/main/java/com/dreamteam/service/SpeciesService.java
@@ -0,0 +1,48 @@
+package com.dreamteam.service;
+
+import com.dreamteam.entity.Species;
+
+import java.util.List;
+
+/**
+ * Service layer for data about specieses.
+ * <p>
+ * Created by jan.novak
+ */
+public interface SpeciesService {
+    /**
+     * Creates new Species.
+     *
+     * @param species new Species
+     */
+    void createSpecies(Species species);
+
+    /**
+     * Deletes existing species.
+     *
+     * @param species species to remove
+     */
+    void deleteSpecies(Species species);
+
+    /**
+     * UPdates species.
+     *
+     * @param species species to update
+     */
+    void updateSpecies(Species species);
+
+    /**
+     * Finds species by its id.
+     *
+     * @param id
+     * @return Species if it exists, NULL otherwise
+     */
+    Species getSpeciesById(long id);
+
+    /**
+     * Returns all existing specieses.
+     *
+     * @return List with all existing specieses.
+     */
+    List<Species> getAllSpecieses();
+}
---
+++ service/src/main/java/com/dreamteam/service/facade/SpeciesFacadeImpl.java
@@ -0,0 +1,142 @@
+package com.dreamteam.service.facade;
+
+import com.dreamteam.dto.SpeciesDTO;
+import com.dreamteam.entity.Animal;
+import com.dreamteam.entity.Species;
+import com.dreamteam.exceptions.TigrAPIException;
+import com.dreamteam.facade.SpeciesFacade;
+import com.dreamteam.service.AnimalService;
+import com.dreamteam.service.SpeciesService;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by jan.novak
+ */
+@Service
+@Transactional
+public class SpeciesFacadeImpl implements SpeciesFacade {
+
+    @Inject
+    private SpeciesService speciesService;
+    @Inject
+    private AnimalService animalService;
+
+    @Override
+    public void createSpecies(SpeciesDTO speciesDTO) {
+        speciesService.createSpecies(convertDtoToSpecies(speciesDTO));
+    }
+
+    @Override
+    public void deleteSpecies(long speciesId) {
+        speciesService.deleteSpecies(getSpeciesByIdOrFailIfNotFound(speciesId));
+    }
+
+    @Override
+    public void changeSpeciesName(long speciesId, String newName) {
+        Species species = getSpeciesByIdOrFailIfNotFound(speciesId);
+        species.setName(newName);
+        speciesService.updateSpecies(species);
+    }
+
+    @Override
+    public void changeSpeciesDescription(long speciesId, String newDescription) {
+        Species species = getSpeciesByIdOrFailIfNotFound(speciesId);
+        species.setDescription(newDescription);
+        speciesService.updateSpecies(species);
+    }
+
+    @Override
+    public void setSpeciesInDanger(long speciesId) {
+        Species species = getSpeciesByIdOrFailIfNotFound(speciesId);
+        species.setInDanger(Boolean.TRUE);
+        speciesService.updateSpecies(species);
+    }
+
+    @Override
+    public void setSpeciesNotInDanger(long speciesId) {
+        Species species = getSpeciesByIdOrFailIfNotFound(speciesId);
+        species.setInDanger(Boolean.FALSE);
+        speciesService.updateSpecies(species);
+    }
+
+    @Override
+    public void addAnimalIntoSpecies(long animalId, long speciesId) {
+        Species species = getSpeciesByIdOrFailIfNotFound(speciesId);
+        Animal animal = animalService.findById(animalId);
+
+        //store into animal - owning side
+        animal.setSpecies(species);
+        animalService.update(animal);
+
+        //store into collection - inverse side
+        species.addAnimal(animal);
+        speciesService.updateSpecies(species);
+    }
+
+    @Override
+    public void removeAnimalFromSpecies(long animalId, long speciesId) {
+        Species species = getSpeciesByIdOrFailIfNotFound(speciesId);
+        Animal animal = animalService.findById(animalId);
+
+        //store into animal - owning side
+        animal.setSpecies(null);
+        animalService.update(animal);
+
+        //store into collection - inverse side
+        species.removeAnimal(animal);
+        speciesService.updateSpecies(species);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public SpeciesDTO getSpeciesById(long id) {
+        return convertSpeciesToDto(getSpeciesByIdOrFailIfNotFound(id));
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<SpeciesDTO> getAllSpecieses() {
+        return convertSpeciesListToDtosList(speciesService.getAllSpecieses());
+    }
+
+    private Species getSpeciesByIdOrFailIfNotFound(long id) {
+        Species species = speciesService.getSpeciesById(id);
+        if (species == null)
+            throw new TigrAPIException("Cannot found species with ID: " + id);
+        return species;
+    }
+
+    private Species convertDtoToSpecies(SpeciesDTO dto) {
+        Species species = new Species();
+        species.setId(dto.getId());
+        species.setName(dto.getName());
+        species.setDescription(dto.getDescription());
+        //todo species.setAnimals();
+        species.setInDanger(dto.isInDanger());
+        return species;
+    }
+
+    private SpeciesDTO convertSpeciesToDto(Species species) {
+        SpeciesDTO dto = new SpeciesDTO();
+        dto.setId(species.getId());
+        dto.setName(species.getName());
+        dto.setDescription(species.getDescription());
+        //todo dto.setAnimals(species);
+        dto.setInDanger(species.isInDanger());
+        return dto;
+    }
+
+    private List<SpeciesDTO> convertSpeciesListToDtosList(List<Species> speciesList) {
+        List<SpeciesDTO> dtos = new ArrayList<>();
+        for (Species species : speciesList) {
+            dtos.add(convertSpeciesToDto(species));
+        }
+        return dtos;
+    }
+}
