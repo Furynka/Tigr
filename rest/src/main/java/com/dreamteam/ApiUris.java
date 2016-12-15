bbc894fafd4362a83ae82c04bc7ecd3c6cb778c9
evaambrusova <eva.ambrus94@gmail.com>
2016-12-15 14:42:34 +0100

    Created MainController. WorkerController updated.

---
+++ rest/src/main/java/com/dreamteam/ApiUris.java
@@ -0,0 +1,8 @@
+package com.dreamteam;
+
+/**
+ * @author Eva Ambrusova
+ */
+public abstract class ApiUris {
+    public static final String ROOT_URI_WORKER = "/worker";
+}
--- rest/src/main/java/com/dreamteam/controllers/WorkerController.java
+++ rest/src/main/java/com/dreamteam/controllers/WorkerController.java
@@ -1,11 +1,35 @@
 package com.dreamteam.controllers;
 
+import com.dreamteam.ApiUris;
+import com.dreamteam.dto.WorkerDTO;
+import com.dreamteam.facade.WorkerFacade;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.inject.Inject;
+import java.util.Collection;
+
 /**
  * REST controller for Worker
  *
  * @author Eva Ambrusova
  */
-
+@RestController
+@RequestMapping(ApiUris.ROOT_URI_WORKER)
 public class WorkerController {
 
+    final static Logger logger = LoggerFactory.getLogger(WorkerController.class);
+
+    @Inject
+    private WorkerFacade workerFacade;
+
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public final Collection<WorkerDTO> getWorkers(){
+        logger.debug("rest getWorkers()");
+        return workerFacade.getAllWorkers();
+    }
 }
